ff7a4ff3974028bdad288e384372cc631d1e8f23
SeTheory <476017949@qq.com>
2022-03-30 15:35:27 +0800

    update

--- predict.py
+++ predict.py
@@ -4,16 +4,17 @@ from data_processor import DataProcessor
 from model import ChineseBERT
 import pandas as pd
 
-path = ''
+path = './checkpoints/ChineseBERT_3.pkl'
 dataProcessor = DataProcessor(0.7)
 tokenizer = dataProcessor.tokenizer
+
 model = ChineseBERT(2).load_model(path)
 model.eval()
 
-df = pd.read_csv('')
-contents = df['review'].to_list()
+df = pd.read_csv('./data/predicted_data.csv')
+contents = df['评论内容'].to_list()
 contents = [dataProcessor.get_dealt_text(content, 256) for content in contents]
 with torch.no_grad():
-    results = [model(content[0], content[1]) for content in contents]
+    results = [model(content[0], content[1]).numpy().tolist() for content in contents]
 df['result'] = results
 df.to_csv('predicted_result.csv')
